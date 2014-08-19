105865883b9facc7c763c007cb8fd2b9bbf29b4f
smoogipooo <smoogipooo@gmail.com>
2014-08-19 18:55:36 +0930

    Added DB to use in file encrypting.

--- api/Helpers/MysqlConfig.php
+++ api/Helpers/MysqlConfig.php
@@ -1,7 +1,10 @@
 <?php
+//Please the following command to make git ignore changes to this file
+//git update-index --assume-unchanged osu\!/Constants/General_Version.cs
 
 if (!defined('DB_HOST')) define('DB_HOST','localhost');
 if (!defined('DB_USER')) define('DB_USER','root');
 if (!defined('DB_PASS')) define('DB_PASS','root');
 
+if (!defined('DB_SALT')) define('DB_SALT', '');
 ?>
