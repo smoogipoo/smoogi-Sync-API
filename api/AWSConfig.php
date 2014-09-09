0ccf84b68f47d1c35c40c77ba216e1c9bc4e299b
smoogipooo <smoogipooo@gmail.com>
2014-09-09 18:21:27 +0930

    Upload files to S3 and add ability to get all user's files

---
+++ api/AWSConfig.php
@@ -0,0 +1,19 @@
+<?php
+
+return array
+(
+    'includes' => array( '_aws' ),
+    'services' => array(
+        'default_settings' => array
+        (
+            'params' => array
+            (
+                'key'    => '',
+                'secret' => '',
+                'region' => 'us-west-2'
+            )
+        )
+    )
+);
+
+?>
