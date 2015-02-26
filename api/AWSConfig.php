5d8db7dda0d565695a6c1e3e3380b6fb541e7b98
smoogipooo <smoogipooo@gmail.com>
2015-02-27 03:34:50 +1030

    Remove AWS.

--- api/AWSConfig.php
+++
@@ -1,9 +0,0 @@
-<?php
-//Please run the following command to make git ignore changes to this file
-//git update-index --assume-unchanged AWSConfig.php
-
-if (!class_exists('S3'))require_once('Helpers/AWS/S3.php');
-
-$s3Client = new S3('', '');
-
-?>
