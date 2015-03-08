7d18e96a3676c00ad66acdf44a849393056af577
smoogipooo <smoogipooo@gmail.com>
2015-03-09 04:28:16 +1030

    [p] Add bucket sync to the Distribute cronjob.

--- api/Distribute.php
+++ api/Distribute.php
@@ -34,6 +34,64 @@ function distributeFile($file)
 	}
 }
 
+/*
+ * Synchronizes the files between all buckets.
+ */
+function reSync()
+{
+	global $s3Client;
+	global $Buckets;
+
+	//Get the initial list of files from all buckets as array(bucketname => array(files))
+	$bucketFiles = array();
+	foreach ($Buckets as $b)
+	{
+		$files = array();
+		foreach ($s3Client->getBucket($b) as $name => $value)
+			$files[$value['name']] = $value;
+		$bucketFiles[$b] = $files;
+	}
+
+	//Compare buckets, for all missing files,
+	//create an array(fromBucket => array(toBucket => file))
+	$diff = array();
+	for ($b = 0; $b < count($Buckets); $b++)
+	{
+		for ($bNext = $b + 1; $bNext < count($Buckets); $bNext++)
+		{
+			//Files missing in Buckets[b]
+			$missingFiles = getDiff($bucketFiles[$Buckets[$b]], $bucketFiles[$Buckets[$bNext]]);
+			if (count($missingFiles) != 0)
+			{
+				$diff[$Buckets[$b]] = array($Buckets[$bNext] => $missingFiles);
+				$bucketFiles[$Buckets[$b]] = array_merge($bucketFiles[$Buckets[$b]], $missingFiles);
+			}
+
+			//Files missing in Buckets[bNext]
+			$missingFiles = getDiff($bucketFiles[$Buckets[$bNext]], $bucketFiles[$Buckets[$b]]);
+			if (count($missingFiles) != 0)
+			{
+				$diff[$Buckets[$bNext]] = array($Buckets[$b] => $missingFiles);
+				$bucketFiles[$Buckets[$bNext]] = array_merge($bucketFiles[$Buckets[$bNext]], $missingFiles);
+			}
+		}
+	}
+
+	//Copy missing files over between buckets
+	foreach ($diff as $fromBucket => $toArray)
+		foreach ($toArray as $toBucket => $files)
+			foreach ($files as $file)
+				$s3Client->copyObject($fromBucket, $file['name'], $toBucket, $file['name']);
+}
+
+function getDiff($list1, $list2)
+{
+	return array_diff(array_merge($list1, $list2), $list2);
+}
+
+//Distribute initial files
 distributeFolder($UploadPath);
 
+//Resync all buckets
+reSync();
 ?>
