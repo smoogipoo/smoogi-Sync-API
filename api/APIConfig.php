419124ab1a55217aee272827d07bb382776b63eb
smoogipooo <smoogipooo@gmail.com>
2015-03-19 03:02:24 +1030

    Move AWS constants into APIConfig.php.

--- api/APIConfig.php
+++ api/APIConfig.php
@@ -1,6 +1,11 @@
 <?php
 if (!defined('BASE_PATH')) define('BASE_PATH', dirname(__FILE__));
+
 if (!defined('UPLOAD_PATH')) define('UPLOAD_PATH', BASE_PATH . '/../Store');
 if (!defined('UPLOAD_PATH_FILE')) define('UPLOAD_PATH_FILE', UPLOAD_PATH . '/%s');
+
 if (!defined('TEMP_PATH')) define('TEMP_PATH', BASE_PATH . '/../tmp');
 if (!defined('TEMP_PATH_FILE')) define('TEMP_PATH_FILE', TEMP_PATH . '/%s');
+
+if (!defined('AWS_KEY')) define('AWS_KEY', '');
+if (!defined('AWS_SECRET')) define('AWS_SECRET', '');
--- api/Helpers/S3/S3Config.php
+++ api/Helpers/S3/S3Config.php
@@ -3,7 +3,4 @@
 //Please run the following command to make git ignore changes to this file
 //git update-index --assume-unchanged S3Config.php
 
-if (!defined('AWS_KEY')) define('AWS_KEY', '');
-if (!defined('AWS_SECRET')) define('AWS_SECRET', '');
-
 static $Buckets = array('');
