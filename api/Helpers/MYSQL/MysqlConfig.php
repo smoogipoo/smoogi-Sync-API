2c31ddd8c331aaccdd8ae99fdacec490c336cc5f
smoogipooo <smoogipooo@gmail.com>
2015-03-19 03:02:56 +1030

    Update to unix line endings.

--- api/Helpers/MYSQL/MysqlConfig.php
+++ api/Helpers/MYSQL/MysqlConfig.php
@@ -2,9 +2,9 @@
 //Please run the following command to make git ignore changes to this file
 //git update-index --assume-unchanged MysqlConfig.php
 
-if (!defined('DB_HOST')) define('DB_HOST', 'localhost');
-if (!defined('DB_USER')) define('DB_USER', 'root');
-if (!defined('DB_PASS')) define('DB_PASS', 'root');
+if (!defined('DB_HOST')) define('DB_HOST', '');
+if (!defined('DB_USER')) define('DB_USER', '');
+if (!defined('DB_PASS')) define('DB_PASS', '');
 
 if (!defined('DB_SALT')) define('DB_SALT', '');
 ?>
